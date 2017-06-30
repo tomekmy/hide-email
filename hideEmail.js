8b239864f7308a83185bec4102d4f49e5174fa65
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2017-06-30 12:26:35 +0200

    Update hideEmail.js
    Add optional tag parameter to put any tag inside <a href="mailto:"></a>

--- hideEmail.js
+++ hideEmail.js
@@ -1,8 +1,12 @@
 (function($) {
-	$.fn.hideEmail = function(user, site) {
+	$.fn.hideEmail = function(user, site, tag) {
 		this.each(function() {
-			var string = '<a href="&#109;&#097;&#105;&#108;&#116;&#111;:'+user+'&#064;'+site+'">'+user+'&#064;'+site+'</a>';
-			$(this).append(string);
+		    if(tag) {
+					var string = '<a href="&#109;&#097;&#105;&#108;&#116;&#111;:'+user+'&#064;'+site+'">'+tag+'</a>';
+		    } else {
+					var string = '<a href="&#109;&#097;&#105;&#108;&#116;&#111;:'+user+'&#064;'+site+'">'+user+'&#064;'+site+'</a>';
+		    }
+		    $(this).append(string);
 		});
 	};
 })(jQuery);
